34a3c1f2c6cde2618496f9afdcae82c903ac200e
Adrian <ortizadrian628@gmail.com>
2018-11-27 10:39:07 -0800

    adrian

---
+++ drugs.js
@@ -0,0 +1,14 @@
+ $(document).ready(function(){
+	$("h1").click(function(){
+		$("p").toggle();
+	} )
+ } ) 
+ 
+  $(document).ready(function(){
+	$("#p1").click(function(){
+		$("img").toggle();
+		$("#p2").toggle();
+		$("#p3").toggle();
+		$("#p4").toggle();
+	} )
+ } ) 
